1a282a864ee810b649ea9c7c39108869cd50c078
Lorena <lorenaborgessilva@gmail.com>
2023-11-20 00:43:13 -0300

    adicionando animes via form

--- scripts.js
+++ scripts.js
@@ -1,15 +1,43 @@
 var listaAnimes = [];
-listaAnimes[0] = 'https://upload.wikimedia.org/wikipedia/pt/b/b2/SwordArtOnlineVol.1.jpg'
-listaAnimes[1] = 'https://m.media-amazon.com/images/I/81yo1ig28HL._AC_UF1000,1000_QL80_.jpg'
-listaAnimes[2] = 'https://m.media-amazon.com/images/M/MV5BNjRiNmNjMmMtN2U2Yi00ODgxLTk3OTMtMmI1MTI1NjYyZTEzXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg'
-listaAnimes[3] = 'https://br.web.img3.acsta.net/c_310_420/pictures/16/04/11/16/56/089875.jpg'
+var listaNomeAnimes = [];
+var listaTrailersAnimes = [];
 
-var listaNomeAnimes= ['Sword Art Online', 'Yona of The Dawn', 'Death Note', 'Naruto'];
-document.write('<div class="container_todosFilmes">')
+function adicionarAnime(){
+  var animeEscolhido = document.getElementById('anime').value;
+  var nomeAnimeEscolhido = document.getElementById('nome').value;
+  var trailerAnime = document.getElementById('trailer').value;
+  
+  
+    if(( animeEscolhido.endsWith('jpg') ) || ( animeEscolhido.endsWith('jpeg') )){
+      
+      document.getElementById('mensagemDeErro').innerHTML = '';
+      listaAnimes.push(animeEscolhido);
+      listaTrailersAnimes.push(trailerAnime);
+      listaNomeAnimes.push(nomeAnimeEscolhido);
+  
+      
+      limpaCampos();
+      recarregarAnimes();
+      
+    } else {
+      
+      document.getElementById('mensagemDeErro').innerHTML = 'Endereço de imagem inválido, tente novamente';
+      limpaCampos();
+      
+    }
+  }
 
-for(var i=0; i<listaAnimes.length; i++){
-    document.write('<div class="container_filme">')
-    document.write('<img src=' + listaAnimes[i] + '>')
-    document.write('<p>'+ listaNomeAnimes[i] +'</p>')
-    document.write('</div>')
+function recarregarAnimes(){
+ 
+  var elementoListaAnimes = document.getElementById('listaAnimes');
+  elementoListaAnimes.innerHTML = '';
+  for(i=0; i < listaAnimes.length ; i++){
+    elementoListaAnimes.innerHTML += `<a href=" ${listaTrailersAnimes[i]} " target="_blank"><img src=" ${listaAnimes[i]} "><p>${listaNomeAnimes[i]}</p></a>`
+  }
+}
+
+function limpaCampos(){
+   document.getElementById('nome').value = '';
+   document.getElementById('anime').value = '';
+   document.getElementById('trailer').value = '';
 }
